c722f73b4e48ca812cd97ff2b8e756d1c445e451
Layanor <98930504+Layanor@users.noreply.github.com>
2022-10-06 21:06:51 +0300

    last one

--- main.py
+++ main.py
@@ -24,42 +24,38 @@ def create_initial_population(items,containers,option):
         container_list.append([])
     #Fill containers with items weight(Genes)
     for i in range(1,items+1):
-        #Select a random container
         index = random.randint(0,containers-1)
-        #Add weight to the container based on user option selecting
         if option == 1:
             container_list[index].append((i/2))
         else:
             container_list[index].append(((i**2)/2))
 
-#function to calculate mean weight difference between containers
+#Function to calculate mean weight difference between containers
 def compute_mean_weight_difference(container):
-    #list to store the sum of weights for each container
+
     each_container_weight_sum = []
-    #iterating till the number of containers
+
     for i in range(0,len(container)):
-        #variable to store sum of each container weights
         sum = 0
-        #iterating till the length of each container
         for j in range (0,len(container[i])):
-            #adding up weights in the container
+            #Calculate the sum of weights in each container
             sum += container[i][j]
-        #appending the weight sum to list
+
         each_container_weight_sum.append(sum)
-    #calculating difference between the total weights of each container
+    #Calculate difference between the sum of weights of each container
     # weight_of_container[i] - weight_of_container[i+1]
-    weight_diff_between_containers = [abs(j-i) for i, j in zip(each_container_weight_sum[:-1], each_container_weight_sum[1:])]
-    #finding the mean difference of weights
+    #weight_diff_between_containers = [abs(j-i) for i, j in zip(each_container_weight_sum[:-1], each_container_weight_sum[1:])]
+    for i, j in zip(each_container_weight_sum[:-1], each_container_weight_sum[1:]):
+        weight_diff_between_containers = [abs(j-i)]
+    #Find the mean difference of weights
     mean_fitness = math.fsum(weight_diff_between_containers)/len(weight_diff_between_containers)
-    #returning the fitness value
     return mean_fitness
 
-#function to find the ideal placement of weights in containers
+#Function to find the ideal placement of weights in containers
 def find_ideal_setting(items,containers):
 
     #Convert the containers list of lists to a 1 dimensional weight list
-
-    #converting the containers list of lists to a 1 dimensional weight list
+    #We have used this source for the list:
     #https://stackoverflow.com/questions/2961983/how-does-sum-flatten-lists
 
     weight_list = [j for item in container_list for j in item]
@@ -96,40 +92,44 @@ def find_ideal_setting(items,containers):
         if j == containers - 1 and (items - element) > 0:
             for i in range(0,items - element):
                 summ = []
-                min_sum = 9999
+                min_sum =0
                 min_index=0
 
-                #Add each remaining items to the minimum container weight
+                #Add each of the remaining items to the minimum container weight
                 for i in range(0,len(ideal_container_list)):
                     summ.append(sum(ideal_container_list[i]) + weight_list[count])
+
                 if element <= count:
-                    #iterating till the length of summ list containing sum of weights of containers
                     for i in range(0,len(summ)):
-                        #if the weight sum at a particular index is less than the min_sum we swap the values
+                        #if the weight sum at an index is less than the min_sum we swap the values
+                        if i == 0:
+                            min_sum=summ[i]
+
                         if summ[i] < min_sum:
                             min_sum = summ[i]
-                            #storing index containing minimum sum
                             min_index = i
-                    #adding the elements weight to the minimum sum index
-                    ideal_container_list[min_index].append(weight_list[count])
-                    #decrementing the count of remaining items
+
+                    ideal_container_list[min_index].append(weight_list[count])                    #decrementing the count of remaining items
                     count -= 1
 
-#function to caluclate fitness
 def calculate_fitness(items,containers):
-    #calling the function to find ideal setting
+    #Call the function to find ideal solution
     find_ideal_setting(items,containers)
     #if the fitness value of our current solution is less than or equal to the fitness threshold value only then its a good distribution of weights across containers
     if compute_mean_weight_difference(container_list) <= compute_mean_weight_difference(ideal_container_list):
-        print("Your containers have a good weight distribution")
-        print("Your solution is :  ",container_list)
-        print("Ideal solution is :  ",ideal_container_list)
+        print("The containers have a good weight distribution")
+        print("The solution is: ",container_list)
+        print("Fitness function of the solution is: ",compute_mean_weight_difference(container_list)  )
+        print("Ideal solution is: ",ideal_container_list)
+
     else:
         print("Weights are not smartly distributed across containers")
-        print("Your solution is :  ",container_list)
-        print("Ideal solution is :  ",ideal_container_list)
+        print("The solution is: ",container_list)
+        print("Fitness function of the solution is: ", compute_mean_weight_difference(container_list))
+        print("Ideal solution is: ",ideal_container_list)
 
-#calling the functions
+
+#Start
 items,containers,option = input_func()
 create_initial_population(items,containers,option)
 calculate_fitness(items,containers)
